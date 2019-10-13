2557c6109dfd333f52abc960dd4ab4926990a84b
Leno <leno@localhost.localdomain>
2019-10-12 22:03:42 -0400

    Scrapped old JS

--- app.js
+++
@@ -1,6 +0,0 @@
-let navRight = document.querySelector("#navbar__right");
-let navRightButton = document.querySelector(".navbar__logo");
-
-navRightButton.addEventListener("click", function () {
-    navRight.classList.toggle("navbar__section--menutoggle");
-});
