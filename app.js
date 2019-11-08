7669cc7162682c229ec20b5d3d4184e91ed997d4
Lenin <lenin.an.ln@gmail.com>
2019-11-08 09:22:25 -0400

    Final touches

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const body = document.querySelector("body");
 const header = document.querySelector("header");
-
-
 const navbar = document.querySelector(".navbar")
+
 let heightOffset = navbar.offsetHeight;
 
 
-window.addEventListener("scroll", fixNav);
+
 
 
 function fixNav() {
-    if (window.scrollY > heightOffset) {
-        header.style.paddingTop = `${heightOffset}px`;
-        body.classList.add("fixed");
-        console.log(heightOffset);
-    } else {
-      body.classList.remove("fixed");
-      header.style.paddingTop = `0px`;
-    }
-  };
-  
+  if (window.scrollY > heightOffset) {
+    header.style.paddingTop = `${heightOffset}px`;
+    body.classList.add("fixed");
+  } else {
+    body.classList.remove("fixed");
+    header.style.paddingTop = `0px`;
+  }
+};
+
+
+window.addEventListener("scroll", fixNav);
